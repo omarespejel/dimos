648437b8b59a7786ad72a3cbd061724e8efebc11
Jeff Hykin <jeff.hykin@gmail.com>
2026-05-25 11:29:17 -0700

    add x2 config.py

---
+++ dimos/robot/agibot/x2_ultra/config.py
@@ -0,0 +1,22 @@
+# Copyright 2026 Dimensional Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
+
+from dimos.robot.config import RobotConfig
+
+X2Config = RobotConfig(
+    name="agibot_x2_ultra",
+    model_path=Path(__file__).parent / "x2_ultra.urdf",
+)
